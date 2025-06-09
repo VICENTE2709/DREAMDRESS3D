ad174abcff866fd7cd3fe53000974fbc44f2ddb7
VICENTE2709 <lpze.vicenteoscar.claros.ma@unifranz.edu.bo>
2025-06-08 21:59:54 -0400

    Add role-based dashboard controllers and views

---
+++ app/Controllers/dashboard_vendedor.php
@@ -0,0 +1,15 @@
+<?php
+require_once(__DIR__ . '/../../core/AuthMiddleware.php');
+require_once(__DIR__ . '/../../core/functions.php');
+
+require_auth();
+
+$usuario = current_user();
+if (!$usuario || !in_array($usuario['RolID'], [4, 1])) {
+    header('Location: ' . urlBase('login'));
+    exit;
+}
+
+render('vendedor/dashboard_vendedor', [
+    'usuario' => $usuario
+]);
